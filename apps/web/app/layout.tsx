7284820bdef6284eadf32887cab71f26bafc882f
reach0908 <wlsghkim0908@gmail.com>
2024-07-22 03:09:38 +0900

    :tada: Project(turborepo): nest, next, tailwind, shadcnui 세팅

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import './globals.css';
+import '@repo/ui/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
