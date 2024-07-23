ad18a17d405bf8de14edffcc5dff726e80f178b0
reach0908 <wlsghkim0908@gmail.com>
2024-07-24 00:50:59 +0900

    :lipstick: Add(storybook): 스토리북 앱 추가

---
+++ apps/storybook/.storybook/preview.ts
@@ -0,0 +1,27 @@
+import '@repo/ui/styles/globals.css';
+import { withThemeByClassName } from '@storybook/addon-themes';
+
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+	decorators: [
+		withThemeByClassName({
+			themes: {
+				light: 'light',
+				dark: 'dark',
+			},
+			defaultTheme: 'light',
+		}),
+	],
+};
+
+export default preview;
